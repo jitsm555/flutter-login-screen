ea2350ada3191fa59495f5bad3caf07befe41c6b
Jitesh-Mohite <jitesh.mohite@globallogic.com>
2019-05-23 23:43:07 +0530

    initial login page

---
+++ lib/login_page.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+class LoginPage extends StatefulWidget {
+  static String tag = 'login-page';
+
+  @override
+  _LoginPageState createState() => new _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    Map<int, Color> color = {
+      50: Color.fromRGBO(147, 205, 72, .1),
+      100: Color.fromRGBO(147, 205, 72, .2),
+      200: Color.fromRGBO(147, 205, 72, .3),
+      300: Color.fromRGBO(147, 205, 72, .4),
+      400: Color.fromRGBO(147, 205, 72, .5),
+      500: Color.fromRGBO(147, 205, 72, .6),
+      600: Color.fromRGBO(147, 205, 72, .7),
+      700: Color.fromRGBO(147, 205, 72, .8),
+      800: Color.fromRGBO(147, 205, 72, .9),
+      900: Color.fromRGBO(147, 205, 72, 1),
+    };
+    MaterialColor colorCustom = MaterialColor(0xFF93cd48, color); // green color
+
+    final logo = Hero(
+      tag: 'hero',
+      child: CircleAvatar(
+        backgroundColor: Colors.transparent,
+        radius: 48.0,
+        child: Image.asset('images/logo.png'),
+      ),
+    );
+
+    final email = TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      initialValue: 'alucard@gmail.com',
+      decoration: InputDecoration(
+        hintText: 'Email',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+    final password = TextFormField(
+      autofocus: false,
+      initialValue: 'some password',
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Password',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+    final loginButton = Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+        onPressed: () {
+          Navigator.of(context).pushNamed(HomePage.tag);
+        },
+        padding: EdgeInsets.all(12),
+        color: colorCustom,
+        child: Text('Log In', style: TextStyle(color: Colors.white)),
+      ),
+    );
+
+    final forgotLabel = FlatButton(
+      child: Text(
+        'Forgot password?',
+        style: TextStyle(color: Colors.black54),
+      ),
+      onPressed: () {},
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.only(left: 24.0, right: 24.0),
+          children: <Widget>[
+            logo,
+            SizedBox(height: 48.0),
+            email,
+            SizedBox(height: 8.0),
+            password,
+            SizedBox(height: 24.0),
+            loginButton,
+            forgotLabel
+          ],
+        ),
+      ),
+    );
+  }
+}
